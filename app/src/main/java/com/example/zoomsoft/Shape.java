fda55d4e9447ab76b7d769ad2a83ee39652bab07
Kingsley <nkokeke@ualberta.ca>
2021-10-04 00:13:59 -0600

    "My shape"

--- app/src/main/java/com/example/zoomsoft/Shape.java
+++ app/src/main/java/com/example/zoomsoft/Shape.java
@@ -1,5 +1,6 @@
 package com.example.zoomsoft;
 
-public abstract class Shape { int x; int y;
-    String color = "$Red";
+public abstract class Shape {
+    int x; int y;
+    String color = "brown";
 }
