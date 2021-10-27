ad39fe1fc6f94fd4b50f996c2f07dcd1e518f2ee
Asad Idrees <asadidrees70@gmail.com>
2021-10-27 00:02:32 -0600

    added UI for login registration and main activity and the base class for each

--- app/src/main/java/com/example/zoomsoft/MainActivity.java
+++ app/src/main/java/com/example/zoomsoft/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.zoomsoft;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button loginButton = findViewById(R.id.login);
+        final Button registerButton = findViewById(R.id.register);
+
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent login = new Intent(getApplicationContext(),login.class);
+                startActivity(login);
+            }
+        });
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent register = new Intent(getApplicationContext(),register.class);
+                startActivity(register);
+            }
+        });
     }
 }
