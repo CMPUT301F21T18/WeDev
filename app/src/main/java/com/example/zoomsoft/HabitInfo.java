e7c3a4573d06d8ef9f51582ff940f1450a9a5240
Asad Idrees <asadidrees70@gmail.com>
2021-11-01 16:31:34 -0600

    fixed the habit info file

--- app/src/main/java/com/example/zoomsoft/HabitInfo.java
+++ app/src/main/java/com/example/zoomsoft/HabitInfo.java
@@ -1,2 +1,41 @@
-package com.example.zoomsoft;public class HabitInfo {
+package com.example.zoomsoft;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.zoomsoft.databinding.ActivityHabitInfoBinding;
+import com.example.zoomsoft.ui.main.SectionsPagerAdapter;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
+
+public class HabitInfo extends AppCompatActivity {
+
+    private ActivityHabitInfoBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityHabitInfoBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = binding.viewPager;
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = binding.habitInfoTabs;
+        tabs.setupWithViewPager(viewPager);
+        FloatingActionButton fab = binding.fabDelete;
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
 }
