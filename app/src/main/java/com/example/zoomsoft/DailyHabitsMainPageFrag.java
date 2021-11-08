86f84d5a79b41f59c006063aa45a886b4c5983f9
Kingsley Okeke <nkokeke@ualberta.ca>
2021-11-08 10:32:12 -0700

    Connected daily habits fragment

--- app/src/main/java/com/example/zoomsoft/DailyHabitsMainPageFrag.java
+++ app/src/main/java/com/example/zoomsoft/DailyHabitsMainPageFrag.java
@@ -10,6 +10,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -81,7 +82,13 @@ public class DailyHabitsMainPageFrag extends Fragment {
                             habitAdaptor.notifyDataSetChanged();
                         }
                     });
-
+            habitList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                    Intent intent = new Intent(getActivity(), HabitInfo.class);
+                    startActivity(intent);
+                }
+            });
             return view;
 
             //return inflater.inflate(R.layout.list_of_habits_main_page_fragment, container, false);
