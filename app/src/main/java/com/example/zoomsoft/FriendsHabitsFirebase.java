f3af4a0d77b53bbfa103963bb9b9424d51379d02
Ola Raji <oraji@ualberta.ca>
2021-11-25 21:11:27 -0800

    Friends habits

--- app/src/main/java/com/example/zoomsoft/FriendsHabitsFirebase.java
+++ app/src/main/java/com/example/zoomsoft/FriendsHabitsFirebase.java
@@ -31,81 +31,28 @@ public class FriendsHabitsFirebase extends AppCompatActivity {
 
     }
     public void getFriendsHabits(FriendsHabitsInterface friendsHabitsInterface){
-        final CollectionReference collectionReference = db.collection("Habits");
-        DocumentReference documentReference = collectionReference.document(email);
-        documentReference.get(source).addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if(task.isSuccessful()) {
-                    DocumentSnapshot documentSnapshot = task.getResult();
-                    if (documentSnapshot.exists()) {
-                        Map<String, Object> map = documentSnapshot.getData();
-                        Log.d("Map provided: ", map.toString());
-                        ArrayList<String>  friendsHabits = (ArrayList<String>) map.get("habits"); //an arraylist of habits
-                        friendsHabitsInterface.callBackFriendsHabits(friendsHabits);
+        final CollectionReference collectionReference = db.collection("Events");
+        collectionReference
+                .document(email)
+                .get(source)
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()){
+                            DocumentSnapshot documentSnapshot = task.getResult();
+                            if(documentSnapshot.exists()){
+                                Map<String, Object> map = documentSnapshot.getData();
+                                ArrayList<String> friendsHabitsList = new ArrayList<>(map.keySet());
+                                friendsHabitsInterface.callBackFriendsHabits(friendsHabitsList);
+                            }
+                        }else{
+
+                        }
                     }
-                }
-            }
-        });
+                });
+
     }
 }
 
 
 
-
-
-
-//    ListView friendsHabitsList;
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState ) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.);
-//
-//        FriendsFirebase friendsFirebase = new FriendsFirebase();
-//        friendsFirebase.getFriend(new FriendsFirebase.FriendsInterface() {
-//            @Override
-//            public void callBackFriends(ArrayList<String> friends) {
-//                FriendsArrayAdapter friendsArrayAdapter = new FriendsArrayAdapter(getApplicationContext(), );
-//                friendsHabitsList = findViewById(R.id.);
-//                friendsHabitsList.setAdapter(friendsArrayAdapter);
-//            }
-//        });
-//    }
-
-
-
-//    ListView friendsListView;
-//    FirebaseFirestore db = FirebaseFirestore.getInstance();
-//
-//    interface FriendsInterface{
-//        void callBackFriends(ArrayList<String> friends );
-//    }
-//
-//    public FriendsFirebase() {
-//
-//    }
-//
-//    /**
-//     * Gets the list of friends for each user.
-//     * @param friendsInterface
-//     */
-//
-//    public void getFriend(FriendsInterface friendsInterface){
-//        final CollectionReference collectionReference = db.collection("Friends");
-//        DocumentReference documentReference = collectionReference.document(email);
-//        documentReference.get(source).addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-//            @Override
-//            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-//                if(task.isSuccessful()) {
-//                    DocumentSnapshot documentSnapshot = task.getResult();
-//                    if (documentSnapshot.exists()) {
-//                        Map<String, Object> map = documentSnapshot.getData();
-//                        Log.d("Map provided: ", map.toString());
-//                        ArrayList<String>  friends = (ArrayList<String>) map.get("friends"); //an arraylist of friends
-//                        friendsInterface.callBackFriends(friends);
-//                    }
-//                }
-//            }
-//        });
-//    }
-//}
