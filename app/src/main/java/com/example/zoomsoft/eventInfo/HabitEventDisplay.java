0ac698819ea391dbd653c273b01c4b7e0e86786f
RomanKyrzyk <60055088+RomanKyrzyk@users.noreply.github.com>
2021-11-25 19:46:26 -0700

    Now the camera updates

--- app/src/main/java/com/example/zoomsoft/eventInfo/HabitEventDisplay.java
+++ app/src/main/java/com/example/zoomsoft/eventInfo/HabitEventDisplay.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.util.Log;
 import android.view.LayoutInflater;
