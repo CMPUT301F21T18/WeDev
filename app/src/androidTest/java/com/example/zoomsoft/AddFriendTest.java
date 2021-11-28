22fdf2841a67f48f4b65a136b6ffcc7efce3db02
Ola Raji <oraji@ualberta.ca>
2021-11-27 17:21:41 -0800

    Added add friend test

---
+++ app/src/androidTest/java/com/example/zoomsoft/AddFriendTest.java
@@ -0,0 +1,148 @@
+package com.example.zoomsoft;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import android.Manifest;
+import android.app.Activity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.rule.GrantPermissionRule;
+
+import com.example.zoomsoft.loginandregister.Login;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.robotium.solo.Solo;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+/**
+ * Test class for Add Friend Activity. All the UI tests are written here. Robotium test framework is
+ used.
+ UI tested: change in activity from Main to received req and verify the data in database and data structure
+ */
+@RunWith(AndroidJUnit4.class)
+public class AddFriendTest {
+    private Solo solo;
+    @Rule
+    public ActivityTestRule<MainActivity> rule =
+            new ActivityTestRule<>(MainActivity.class, true, true);
+
+    // grant permission
+    @Rule
+    public GrantPermissionRule mRuntimePermissionRule = GrantPermissionRule.grant(Manifest.permission.CAMERA);
+
+    /**
+     * Runs before all tests and creates solo instance.
+     * @throws Exception
+     */
+    @Before
+    public void setUp() throws Exception{
+        solo = new Solo(InstrumentationRegistry.getInstrumentation(),rule.getActivity());
+    }
+
+    /**
+     * Gets the Activity
+     * @throws Exception
+     */
+    @Test
+    public void start() throws Exception{
+        Activity activity = rule.getActivity();
+    }
+
+
+    /**
+     * Tests the add Friend to see if the user exists in the databse
+     * and sends friend request
+     */
+    @Test
+    public void receivedRequestTest(){
+        FirebaseFirestore db;
+        //Asserts that the current activity is the MainActivity. Otherwise, show “Wrong Activity”
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+        // Go to next activity login
+        solo.clickOnButton("Login");
+        solo.assertCurrentActivity("Wrong Activity", Login.class);
+
+        // enter the data and test
+        solo.enterText((EditText) solo.getView(R.id.email), "asad@gmail.com");
+        solo.enterText((EditText) solo.getView(R.id.password), "123456");
+        solo.clickOnButton("Login");
+
+        // check if activity switched properly
+        solo.assertCurrentActivity("Wrong Activity", MainPageTabs.class);
+
+        // profile tab
+        solo.clickOnText("Profile");
+
+        // add friend
+        solo.clickOnText("Add Friend");
+        solo.assertCurrentActivity("Wrong Activity", AddFriends.class);
+
+        solo.enterText((EditText) solo.getView(R.id.add_friend_field), "you@gmail.com");
+        solo.clickOnButton("Add Friend");     // email shouldn't exist
+
+//        // back to profile tab
+//        solo.goBack();
+//        solo.assertCurrentActivity("Wrong Activity", MainPageTabs.class);
+
+        // check if the testing email is in Firebase
+
+        db = FirebaseFirestore.getInstance();
+        final CollectionReference collectionReference = db.collection("User");
+        final String userName = "asad@gmail.com";
+        if (userName.length() > 0) {
+            collectionReference
+                    .document(userName)
+                    .get()
+                    .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                            if (task.getResult().exists()) {
+                                assertTrue(solo.searchText("asad@gmail.com"));
+                            } else {
+                                assertFalse(solo.searchText("asad@gmail.com"));
+                            }
+                        }
+                    });
+        }
+
+        // back to profile tab
+        solo.goBack();
+        solo.assertCurrentActivity("Wrong Activity", MainPageTabs.class);
+
+    }
+
+    /**
+     * Close activity after each test
+     * @throws Exception
+     */
+    @After
+    public void tearDown() throws Exception{
+        solo.finishOpenedActivities();
+    }
+}
+
+
+
+
